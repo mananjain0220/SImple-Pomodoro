ee647e0c4d8fa30bcd6b3f5c70151b4c5bed0ce8
mananjain0220 <mananjain0220@gmail.com>
2025-02-08 16:40:00 +0100

    Initial Commit

---
+++ Simple-Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple-Pomodoro
+//
+//  Created by manan jain on 08/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple-Pomodoro/Simple_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_PomodoroApp.swift
+//  Simple-Pomodoro
+//
+//  Created by manan jain on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
